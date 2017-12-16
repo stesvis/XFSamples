29f9bf83494424cc83153017858e35e4ed195fe6
Cristian Merli <stesvis@gmail.com>
2017-12-15 18:23:14 -0700

    Add project files.

---
+++ XFSamples/XFSamples/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+
+namespace XFSamples.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private ObservableCollection<MyImage> _images;
+        public ObservableCollection<MyImage> Images
+        {
+            get { return _images; }
+            set { SetProperty(ref _images, value); }
+        }
+
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+
+        public override void OnNavigatedTo(NavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            Images = new ObservableCollection<MyImage>
+            {
+                new MyImage
+                {
+                    Title="Random Image",
+                    Url = "http://perlich.auction/userfile/lot/872AB4A1-84C2-473C-8AB4-E01830C29C30.jpg"
+                }
+            };
+        }
+    }
+
+    public class MyImage
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+    }
+}
